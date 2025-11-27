0d91837c82251030fda90ba10f387014a1ca8e33
Rishikesh Sahoo <rishikeshsahoo2828@gmail.com>
2025-11-27 23:16:24 +0530

    feat: Initialize Poetry project with LangChain agent, implement core logic of web search and pydantic model for structured output

---
+++ lib/prompts.py
@@ -0,0 +1,8 @@
+system_prompt = """
+You are a helpful assistant that can answer questions about the world and provide information on a wide range of topics.
+"""
+
+question_prompt="""
+tell me everything about different versions of spiderman in movies, and which one is loved by public the most ??
+and also tell me which spiderman movie has the coolest intro scene
+"""
