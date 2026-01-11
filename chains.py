53037fdbdfcd2eef70d82cfa5c2b49453369e9af
Rishikesh Sahoo <rishikeshsahoo2828@gmail.com>
2026-01-11 19:09:26 +0530

    initial setup

---
+++ chains.py
@@ -0,0 +1,7 @@
+import datetime
+from dotenv import load_dotenv
+load_dotenv()
+from langchain_core.prompts import MessagesPlaceholder, ChatPromptTemplate
+from langchain_core.messages import HumanMessage
+
+
--- lib/__init__.py
+++
@@ -1,4 +0,0 @@
-from .prompts import system_prompt, question_prompt
-
-
-__all__ = ["system_prompt", "question_prompt"]
